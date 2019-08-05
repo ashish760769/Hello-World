6f076ab6f95dc23d95b90f820b6504019211e7c3
Ashish Shukla <ashishfun008@gmail.com>
2019-08-05 18:20:30 +0530

    add one pattern program

---
+++ pattern1.c
@@ -0,0 +1,29 @@
+/*This program prints the below pattern in n lines
+where n is user input
+*
+**
+***
+****
+*****
+******
+
+Above pattern is for n == 6 */
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    return 0;
+    getchar();
+    
+}
